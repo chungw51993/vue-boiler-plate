a555606101a01fcddf8a4aee76afcf4057cc7ad3
Won Jin Chung <chungw51993@gmail.com>
2019-08-27 14:59:29 -0500

    Added devtool and vuetify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue';
 import VueMeta from 'vue-meta';
+import Vuetify from 'vuetify/lib';
 
 import store from './store';
 import router from './router';
 import App from './App.vue';
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = process.env.NODE_ENV === 'development';
+Vue.config.devtools = process.env.NODE_ENV === 'development';
 
 Vue.use(VueMeta);
+Vue.use(Vuetify);
 
 new Vue({
   router,
